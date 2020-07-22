6609c245bba8f436f6a71f35e86dfc54206c8c2c
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-22 17:36:34 +0100

    journey by transport passed

--- models/traveller.js
+++ models/traveller.js
@@ -17,7 +17,7 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
